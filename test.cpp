5c0494f024e2bc6ab541de0b234d02802aefbc3a
lw <cui@hellolw.com>
2016-03-14 21:00:31 +0800

    perfect gtest setup

--- test.cpp
+++ test.cpp
@@ -1,30 +1,36 @@
 #include "Skiplist.h"
 #include "gtest/gtest.h"
 #include <algorithm>
+#include <iostream>
 using namespace std;
 
 class SkiplistTest: public ::testing::Test {
 protected:
-	static const int LEN = 100000;
+	virtual void SetUp() {
+		srand(time(NULL));
+		for (auto &value: data)
+			value = rand();
+		sort(data, data + LEN);
+		end = unique(data, data + LEN) - data;
+	}
+
+	static const int LEN = 10000;
 	Skiplist p;
 	int data[LEN];
 	int end;
 };
 
 TEST_F(SkiplistTest, insertAndFind) {
-	srand(time(NULL));
-	for (auto &value: data)
-		value = rand();
-	sort(data, data + LEN);
-	end = unique(data, data + LEN) - data;
-	for (auto value: data)
-		ASSERT_TRUE(p.insert(value) && p.find(value));
+	for (int i = 0; i < end; i++) {
+		ASSERT_TRUE(p.insert(data[i]));
+		ASSERT_TRUE(p.find(data[i]));
+	}
 }
 
 TEST_F(SkiplistTest, delAndFind) {
-	for (int i = 0; i < end / 2; i++)
-		ASSERT_TRUE(p.del(data[i]) && !p.find(data[i]));
-
-	for (int i = end / 2; i < end; i++)
-		ASSERT_TRUE(p.find(data[i]));
+	for (int i = 0; i < end; i++) {
+		ASSERT_TRUE(p.insert(data[i]));
+		ASSERT_TRUE(p.del(data[i]));
+		ASSERT_TRUE(!p.find(data[i]));
+	}
 }
